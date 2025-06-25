bd7cdd7938aad109acc3b0056fee45b93035e705
Luana vitoria <luanavitoriasantosaraujo8@gmail.com>
2025-06-25 14:18:00 -0300

    usando o validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -16,6 +17,14 @@ async function bootstrap() {
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document); // Acessível em http://localhost:3000/api
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true, // Remova propriedades não decoradas no DTO
+        forbidNonWhitelisted: true, // Retorna erro se enviar propriedades não permetidas
+        transform: true, // Transforma os tipos automaticamente EX:( string -> number )
+      })
+    )
 
     await app.listen(3000);
 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,19 @@
 import { ApiProperty ,PartialType } from "@nestjs/swagger"
-
+import { IsEmail, IsNotEmpty } from "class-validator"
 export class CreateUserDto {
     
     @ApiProperty({
         example:'Luana vitoria',
          description:'Nome completo do usuário'
     })
+    @IsNotEmpty({ message:'O nome é obrigatorio!'})
     name: string
 
     @ApiProperty({
         example:'luanavitoria@gmail.com',
         description:'Email do usuário'
     })
+    @IsEmail({}, {message:"O email deve ser um endereço válido"})
     email: string
 }
 
